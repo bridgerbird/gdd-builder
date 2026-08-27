35f673044e832a02f2a0de9a0408220138590d81
bridgerbird <bridgerjbird@gmail.com>
2026-08-27 09:02:24 -0700

    Set up local environment.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 "# gdd-builder" 
 
+Build Game Design Documents in minutes! This web app builds Game Design Documents based on input from an easy to use UI.
+
 This app was created using the help of Claude.ai.
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GDDBuilder from "./GDDBuilder.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GDDBuilder />
+  </React.StrictMode>
+);
